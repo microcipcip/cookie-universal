a399b39301a0bbb3338c83610dfc573dd2074fb0
Salvatore Tedde <microcipcip@gmail.com>
2018-02-22 00:35:26 +0000

    Fixing IE11 transpilation issue

--- packages/cookie-universal/webpack.config.js
+++ packages/cookie-universal/webpack.config.js
@@ -2,26 +2,33 @@ const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'cookie-universal.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'var',
-    library: 'Cookie'
-  },
-  module: {
-    rules: [
-      {
-        test: [/\.js$/],
-        exclude: [/node_modules/],
-        loader: 'babel-loader',
-        options: { presets: ['es2015'] }
-      }
+const createConfig = (target, name) => {
+  return {
+    entry: './index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `${name}.js`,
+      libraryTarget: target,
+      library: 'Cookie'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.js$/],
+          exclude: [/node_modules/],
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin([ 'dist' ]),
+      new UglifyJSPlugin()
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin([ 'dist' ]),
-    new UglifyJSPlugin()
-  ]
+  }
 }
+
+module.exports = [
+  createConfig('var', 'cookie-universal'),
+  createConfig('commonjs2', 'cookie-universal-common')
+]
