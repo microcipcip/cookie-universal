3953cb6a81959dab44ab517b5b44752d6d881fad
Salvatore <microcipcip@gmail.com>
2018-03-15 09:58:25 +0000

    All: improving deploy workflow

--- git-deploy.js
+++ git-deploy.js
@@ -5,26 +5,23 @@ if (process.argv.length !== 3) throw Error(`You passed the wrong number of argum
 if (typeof msg !== 'string') throw Error(`You didn't provide a valid msg`)
 
 const deploy = () => {
-  try {
-    console.log(`Trying to commit to your github repository...`)
+  console.log(`Trying to commit to your github repository...`)
 
-    git
-      .add('./*')
-      .commit(msg)
-      .push(
-        ['-u', 'origin', 'master'],
-        () => {
-          console.log(`Your commits have been successfully pushed to your github repository`)
+  git
+    .add('./*')
+    .commit(msg)
+    .push(
+      ['-u', 'origin', 'master'],
+      () => {
+        console.log(`Your commits have been successfully pushed to your github repository`)
 
-          console.log(`-----------------------------------------------------------`)
-          console.log(`-----------------------------------------------------------`)
+        console.log(`-----------------------------------------------------------`)
+        console.log(`-----------------------------------------------------------`)
 
-          console.log(`Run 'lerna publish' to publish on npm`)
-        }
-      )
-  } catch (err) {
-    console.error('Commit failed:', err)
-  }
+        console.log(`Run 'lerna publish' to publish on npm`)
+      }
+    )
+    .catch((err) => console.error('Commit failed:', err))
 }
 
 deploy()
